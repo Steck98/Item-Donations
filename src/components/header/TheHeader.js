01653186a2070edcdf09c59cbc8635b9e3ee44d1
Steck98 <steczkiewicz1998@Gmail.com>
2023-11-11 16:39:57 +0100

    Navigation

---
+++ src/components/header/TheHeader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavigationMenu from './navigation';
+import SignIn from './signIn';
+
+const Header = () => {
+    return (
+        <div>
+            <SignIn />
+            <NavigationMenu />
+</div>
+    );
+
+}
+export default Header;
